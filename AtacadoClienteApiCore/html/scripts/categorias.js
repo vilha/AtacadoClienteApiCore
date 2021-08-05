58c62952696b3c8a2d462c5d47ab3c78678f5260
vilha <viniciusilhapsg@gmail.com>
2021-08-05 17:26:08 -0400

    Add project files.

---
+++ AtacadoClienteApiCore/html/scripts/categorias.js
@@ -0,0 +1,71 @@
+﻿var categorias = [];
+
+$(function () {
+    if (localStorage.getItem('categorias') == null) {
+        CarregarCategorias();
+    } else {
+        var temp = localStorage.getItem('categorias');
+        categorias = JSON.parse(temp)
+    }
+    PreencherTabelaCategorias();
+});
+
+function CarregarCategorias() {
+    let urlServico = 'http://localhost:37806/api/estoque/categoria';
+    $.ajax({
+        url: urlServico,
+        async: false,
+        success: function (data) {
+            for (let i = 0; i < data.length; i++) {
+                let item = data[i];
+                let categoria = {
+                    categoriaID: item.categoriaID,
+                    descricao: item.descricao,
+                    dataInclusao: item.dataInclusao
+                };
+                categorias.push(categoria);
+
+            }
+            localStorage.setItem('categorias', JSON.stringify(categorias));
+        }
+    });
+    //$.get(urlServico, function(data, status) {
+    //    if (data.length == 0) {
+    //        alert("Erro ao obter os dados de Categoria. Status:" + status)
+    //    } else {
+    //        for (let i = 0; i < data.length; i++) {
+    //            let item = data[i];
+    //            let categoria = {
+    //                categoriaID: item.categoriaID,
+    //                descricao: item.descricao,
+    //                dataInclusao: item.dataInclusao
+    //            };
+    //            categorias.push(categoria);
+    //        }
+    //        localStorage.setItem('categorias', JSON.stringify(categorias));
+    //        PreencherTabelaCategorias();
+    //    }
+    //});
+}
+
+function PreencherTabelaCategorias() {
+    if (categorias == null || categorias.length == 0) {
+        alert("AVISO - os dados de Categorias não foram carregados.");
+        return;
+    } else {
+        for (let i = 0; i < categorias.length; i++) {
+            let item = categorias[i];
+
+            let inicio = '<tr>';
+            let coluna1 = '<td>' + item.categoriaID + '</td>';
+            let coluna2 = '<td>' + item.descricao + '</td>';
+            let coluna3 = '<td>' + item.dataInclusao + '</td>';
+            let coluna4 = '<td>' + /*item. +*/ '</td>';
+            let final = '</tr>';
+
+            let conteudo = inicio + coluna1 + coluna2 + coluna3 + coluna4 + final;
+
+            $('#tblCategorias tbody').append(conteudo);
+        }
+    }
+}
---
+++ AtacadoClienteApiCore/html/scripts/subcategorias.js
@@ -0,0 +1,56 @@
+﻿var subcategorias = [];
+
+$(function () {
+    if (localStorage.getItem('subcategorias') == null) {
+        CarregarSubcategorias();
+    } else {
+        var temp = localStorage.getItem('subcategorias');
+        categorias = JSON.parse(temp)
+    }
+    PreencherTabelaSubcategorias();
+});
+
+function CarregarSubcategorias() {
+    let urlServico = 'http://localhost:37806/api/estoque/subcategoria';
+    $.ajax({
+        url: urlServico,
+        async: false,
+        success: function (data) {
+            for (let i = 0; i < data.length; i++) {
+                let item = data[i];
+                let categoria = {
+                    subcategoriaID: item.subcategoriaID,
+                    categoriaID: item.categoriaID,
+                    descricao: item.descricao,
+                    dataInclusao: item.dataInclusao
+                };
+                subcategorias.push(subcategoria);
+
+            }
+            localStorage.setItem('subcategorias', JSON.stringify(subcategorias));
+        }
+    });
+}
+
+function PreencherTabelaSubcategorias() {
+    if (subcategorias == null || subcategorias.length == 0) {
+        alert("AVISO - os dados de Subcategorias não foram carregados.");
+        return;
+    } else {
+        for (let i = 0; i < subcategorias.length; i++) {
+            let item = subcategorias[i];
+
+            let inicio = '<tr>';
+            let coluna1 = '<td>' + item.subcategoriaID + '</td>';
+            let coluna2 = '<td>' + item.categoriaID + '</td>';
+            let coluna3 = '<td>' + item.descricao + '</td>';
+            let coluna4 = '<td>' + item.dataInclusao + '</td>';
+            let coluna5 = '<td>' + /*item. +*/ '</td>';
+            let final = '</tr>';
+
+            let conteudo = inicio + coluna1 + coluna2 + coluna3 + coluna4 + coluna5 + final;
+
+            $('#tblSubcategorias tbody').append(conteudo);
+        }
+    }
+}
